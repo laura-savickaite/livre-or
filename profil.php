021236bc47437bfcc81b40bce51e6987bac0b34e
Laura Savickaite <elle.@MacBook-Pro-de-Laura.local>
2021-11-23 12:35:22 +0100

    commencement du php pour le profil

--- profil.php
+++ profil.php
@@ -1,2 +1,11 @@
 <!-- Cette page possède un formulaire permettant à l’utilisateur de modifier son
 login et son mot de passe. -->
+
+<?php
+
+session_start();
+$connect = mysqli_connect('localhost', 'root', '', 'livreor');
+
+
+
+?>
