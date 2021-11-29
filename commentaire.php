8d7e3a883a050d99683ea746d5d36439eeb34d95
Laura Savickaite <laura.savickaite@laplateforme.io>
2021-11-29 17:20:54 +0100

    rajout d'images et commentaire css done

--- commentaire.php
+++ commentaire.php
@@ -15,32 +15,22 @@ if(!isset($login)){
 
 $verifid = mysqli_query($connect, "SELECT `id` FROM `utilisateurs` WHERE `login`= '".$login."'");
 $repid = mysqli_fetch_assoc($verifid);
-var_dump($repid);
 
 foreach ($repid as $id) {
-
-   
-    echo $id;
-
-    // si modifier le commentaire maybe ????
-    // $lecommentaire = mysqli_query($connect, "UPDATE `commentaires` SET `commentaire`='$commentaire',`id_utilisateur`='$id'");
-
+    // echo $id;
     }
     
     if(isset($_POST['postcomment'])){
     $commentaire = $_POST['user_comment'];
     $date = date('Y/m/d');
     
-    echo $commentaire;
     $lecommentaire = mysqli_query($connect, 'INSERT INTO `commentaires` (commentaire, id_utilisateur, date) VALUES ("'.$commentaire.'","'.$id.'","'.$date.'")');
-    var_dump($lecommentaire);
 }
 
 
 if (isset($_POST['deco'])){
     session_destroy();
 }
-
 
 ?>
 
@@ -50,17 +40,35 @@ if (isset($_POST['deco'])){
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
     <title>Ajouter un commentaire || Ungodly Hour Radio</title>
 </head>
 <body>
-<form action="commentaire.php" method="post">
+    <header>
+        <section class="navbar">
+            <a href="index.php"><p>Back to the index</p></a>
+            <form action="index.php" method="post">
+                <button class="boutondeco" type="submit" name="deco">Deconnexion</button>
+            </form>
+        </section>
+    </header>
+    <main>
+        <article class="formbackg">
+            <form action="commentaire.php" method="post">
 
-    <label for="name">Ajoutez votre commentaire ici:</label>
-    <input type="text" id="commentaire" name="user_comment">
+                <div class="center">
+                    <label for="name">Ajoutez votre commentaire ici:</label>
+                    <textarea type="text" id="commentaire" name="user_comment" rows="5" cols="33"></textarea>
+                </div>
 
-    <button class="boutonform" type="submit" name="postcomment">Poster mon commentaire</button>
-
-    <button class="boutonform" type="submit" name="deco">deco</button>
-</form>
+                <div class="boutonform">
+                    <button type="submit" name="postcomment">Poster mon commentaire</button>
+                </div>
+            </form>
+        </article>
+        <article class="imgcomm">
+            <img src="Images/imgcomm1.jpeg" width="300px">
+        </article>
+    </main>
 </body>
 </html>
